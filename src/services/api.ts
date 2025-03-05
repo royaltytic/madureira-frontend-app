15cf4fad1d104c2ca56ba3f6ed42959ce6a602a5
Elison Oliveira <royalty.tic13@gmail.com>
2025-03-05 11:06:35 -0300

    config api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", 
+  baseURL: "api-secretaria-agricultura-fn:10000", 
 });
 
 api.interceptors.request.use((config) => {
